694b2113befc61a6c5f698a1e6c157729664a198
dominikjankowski <dominik.jankowsk@gmail.com>
2018-08-09 19:53:22 +0200

    gulp builder

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const gulp          = require('gulp');
+const browserSync   = require('browser-sync');
+const sass          = require('gulp-sass');
+const sourcemaps    = require('gulp-sourcemaps');
+const autoprefixer  = require('gulp-autoprefixer');
+const cleanCSS      = require('gulp-clean-css');
+const uglify        = require('gulp-uglifyes');
+
+
+
+gulp.task('default', ['serve']);
+
+gulp.task('serve',['html', 'sass', 'js'], ()=> {
+
+    browserSync({
+        server: 'public'
+    });
+
+    gulp.watch('src/html/**/*.html', ['html']);
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch('src/js/**/*.js', ['js']);
+
+});
+
+gulp.task('html', ()=> {
+    return gulp.src('src/html/**/*.html')
+        .pipe(gulp.dest('public'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+})
+
+gulp.task('sass', ()=> {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 20 versions']
+        }))
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write())
+        
+        .pipe(gulp.dest('public/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+})
+
+gulp.task('js', ()=> {
+    return gulp.src('src/js/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+})
+
+
+
